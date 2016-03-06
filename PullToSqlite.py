a3e9c1f6bbd1235dd72c30b5cba5d87bdb316158
danikong <bartolot.d@gmail.com>
2016-03-06 18:33:50 +0100

    database handling

--- PullToSqlite.py
+++ PullToSqlite.py
@@ -7,33 +7,31 @@ class sql_handling():
         self.conn = sqlite3.connect('{0}.db'.format(self.db_name))
         self.c = self.conn.cursor()
 
-        self.create_table()
+        #self.create_s_table()
 
     def create_s_table(self):
         self.c.execute("CREATE TABLE IF NOT EXISTS tempSens_s(id INTEGER PRIMARY KEY AUTOINCREMENT, sens_id INTEGER, timestamp REAL, value REAL)")
         self.conn.commit()
 
-#    def create_l_table(self):
-#        self.c.execute("CREATE TABLE IF NOT EXISTS tempSens_s(id INTEGER PRIMARY KEY AUTOINCREMENT, day TEXT, sens_id INTEGER, value REAL)")
-
-    def entry_tempsens_s(self, table_name, value):
-        self.c.execute("INSERT INTO tempSens_s(sens_id, timestamp, value) VALUES (?, ?, ?)",
-                       (id, time.time(), value))
+    def entry_tempsens_s(self, table_name, sens_id, value):
+        self.c.execute("INSERT INTO {0}(sens_id, timestamp, value) VALUES ({1}, {2}, {3})".format(table_name, sens_id, time.time(), value))
         self.conn.commit()
 
-    def readout_tempsens_s(self, table_name):
-        self.c.execute("SELECT * FROM ?"(table_name))
+    def printout_tempsens_s(self, table_name):
+        self.c.execute("SELECT * FROM {0}".format(table_name))
+        data = self.c.fetchall()
+        for row in data:
+            print(row)
 
     def close_db(self):
         self.c.close()
         self.conn.close()
 
-#db_c = sql_handling("temperature")
+db_c = sql_handling("temperature")
 
-for i in range(20):
-    #db_c.entry_tempsens_s(random.randrange(0, 3),random.randrange(0, 30))
-    hello = "hello world"
-    time.sleep(0.5)
+#for i in range(20):
+#    db_c.entry_tempsens_s("tempSens_s" , random.randrange(0, 3), random.randrange(0, 30))
+#    time.sleep(0.5)
 
-
-#db_c.close_db()
+db_c.printout_tempsens_s("tempSens_s")
+db_c.close_db()
